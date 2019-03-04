d533e680321b659f9be97c513abb13749bb8fb4a
Mads Steiner Kristensen <steiner.au@gmail.com>
2019-03-04 13:28:14 +0100

    Added Acefound function

---
+++ [0] Library/AceIsFoundInDrawnCards.m
@@ -0,0 +1,10 @@
+function AcesIsFound = AceIsFoundInDrawnCards( StopPoints, PlayerPoints, PointsDrawn )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+if PlayerPoints + 10 >= StopPoints && PlayerPoints <= 11 && isempty(find(PointsDrawn == 1, 1)) ~= 1
+  AcesIsFound = 1;
+else 
+  AcesIsFound = 0;
+end
+
